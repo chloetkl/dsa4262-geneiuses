5914c92e41dc193a2fce5c5335b073e22f06889f
Ubuntu <ubuntu@ip-172-31-39-36.ap-southeast-1.compute.internal>
2024-10-21 12:26:58 +0000

    added nbstripout to script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Update system packages
 sudo apt update -y
 
-# Install pip for Python 3
 sudo apt install -y python3-pip
 
-# Install Jupyter Notebook
 pip install jupyter
+pip install nbstripout
 
 # Add Jupyter to PATH in .bashrc if not already present
 if ! grep -q 'export PATH=$PATH:/home/ubuntu/.local/bin' ~/.bashrc; then
@@ -16,3 +14,10 @@ fi
 
 # Reload .bashrc to apply PATH changes
 source ~/.bashrc
+
+# Activate nbstripout in the current Git repository
+if [ -d .git ]; then
+    nbstripout --install
+else
+    echo "No Git repository found. Please run this script in a Git repository to activate nbstripout."
+fi
